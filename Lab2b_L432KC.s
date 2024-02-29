f1f3261012d3b00a1ce6cb5c83bad2a8d3a3dcbf
Dion Walton <ddwalton@ualberta.ca>
2024-02-29 11:03:55 -0700

    added part b

---
+++ Lab2b_L432KC.s
@@ -0,0 +1,54 @@
+/*Author - Wing Hoy. Last edited on Jan 14, 2022 */
+/*-----------------DO NOT MODIFY--------*/
+.global TestAsmCall
+.global printf
+.global cr
+.syntax unified
+
+.text
+TestAsmCall:
+PUSH {lr}
+/*--------------------------------------*/
+
+/*-------Students write their code here ------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*-------Code ends here ---------------------*/
+
+/*-----------------DO NOT MODIFY--------*/
+POP {PC}
+
+.data
+/*--------------------------------------*/
